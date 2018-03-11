bfe41cb9ccdfbe3f5c8bb996b90f3d480253f172
cutehamster313 <lastletter313@mail.ru>
2018-03-11 23:33:55 +0300

    Adding start menu page

---
+++ mainmenu.cpp
@@ -0,0 +1,20 @@
+#include "mainmenu.h"
+#include "ui_mainmenu.h"
+
+MainMenu::MainMenu(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::MainMenu)
+{
+    ui->setupUi(this);
+}
+
+MainMenu::~MainMenu()
+{
+    delete ui;
+}
+
+void MainMenu::on_exit_clicked(bool)
+{
+    //this->parent()->findChild<QWidget*>("MainWindow")->close();
+    exit(0);
+}
